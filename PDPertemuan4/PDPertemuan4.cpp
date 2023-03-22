4f10a158a0b52813ad6261cae535ff3bb8590993
Jantopthegreat <fauzangriya@gmail.com>
2023-03-22 15:05:46 +0700

    Add project files.

---
+++ PDPertemuan4/PDPertemuan4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+float Luaspersegipanjang(float p, float l) {
+	return p * l;
+}
+
+float Luassegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
+
+float luaslungkaran(float r)
+
+
+
+
+int main() {
+	float panjang, lebar, tinggi, jejari, alas;
+	int pilihan;
+
+	cout <<
+}
